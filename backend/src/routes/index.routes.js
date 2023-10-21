2d26560f8f0d4de8e8f634e73f74c06955cdd059
Nicolás Opazo <nicolas.opazo2001@alumnos.ubiobio.cl.com>
2023-10-21 19:51:36 -0300

    Actualizacion Index

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -8,8 +8,11 @@ const userRoutes = require("./user.routes.js");
 /** Enrutador de autenticación */
 const authRoutes = require("./auth.routes.js");
 
-//enrutador de evaluaciones
+//Enrutador de evaluaciones
 const evaluacionRoutes = require("./evaluacion.routes.js");
+
+//Enrutador de motivos de rechazo
+const motivorechazoRoutes = require("./motivorechazo.routes.js");
 
 /** Middleware de autenticación */
 const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
@@ -23,6 +26,9 @@ router.use("/users", authenticationMiddleware, userRoutes);
 router.use("/auth", authRoutes);
 // Define las rutas para las evaluaciones /api/evaluaciones
 router.use("/evaluacion", authenticationMiddleware, evaluacionRoutes);
+
+// Define la ruta para los motivos de rechazo /api/motivorechazo
+router.use("/motivorechazo", authenticationMiddleware, motivorechazoRoutes);
 
 // Exporta el enrutador
 module.exports = router;
